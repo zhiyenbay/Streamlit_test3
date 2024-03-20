649b55e886b654c71eee2c6baa78260c9b96d23b
zhiyenbay <zh_olzhas@mail.ru>
2024-03-20 12:39:09 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,19 @@ dotenv.load_dotenv()
 # openai.api_key = os.getenv("asd")
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
+# transcription = client.audio.transcriptions.create(
+#   model="whisper-1", 
+#   file=audio_file
+# )
+
+client = OpenAI()
 def transcribe(audio_file):
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript
+    # transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    return transcript.text
 
 
 def save_audio_file(audio_bytes, file_extension):
